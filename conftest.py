fa019e714709fc81b635431a0e574426bf77e77c
evnlly <133085628+evnlly@users.noreply.github.com>
2023-06-04 15:24:25 +0700

    Delete conftest.py

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-@pytest.fixture
-def max_number():
-    return 5
-def pytest_generate_tests(metafunc):
-    if "max_number" in metafunc.fixturenames:
-    # end can be retrived from command line parameters
-        end = 10
-        metafunc.parametrize("max_number", range(end)
